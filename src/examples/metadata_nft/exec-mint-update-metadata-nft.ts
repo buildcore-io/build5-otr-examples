12af6f52ebde3c905965ac4fc6fcab388b9e7ffd
Boldizsar Mezei <boldizsar@unchained.work>
2023-06-01 19:27:43 +0200

    Fixes

--- src/examples/metadata_nft/exec-mint-update-metadata-nft.ts
+++ src/examples/metadata_nft/exec-mint-update-metadata-nft.ts
@@ -1,29 +1,9 @@
-import { IndexerPluginClient, TransactionHelper } from '@iota/iota.js';
-import config from '../../config.json';
-import { wait } from '../../utils/wait';
-import { getNewWallet } from '../../utils/wallet/Wallet';
-import { getLedgerInclusionState } from '../../utils/wallet/block.utils';
 import { mintMetadataNft } from './mint-metadata-nft';
 
 const exec = async () => {
-  const wallet = await getNewWallet();
-  const sender = await wallet.getIotaAddressDetails(config.mnemonic);
-
-  await mintMetadataNft({ name: 'Test', someValue: 'test' });
-
-  const indexer = new IndexerPluginClient(wallet.client);
-
-  await wait(async () => {
-    const items = (await indexer.nfts({ addressBech32: sender.bech32 })).items;
-    return items.length === 1;
-  });
-  const items = (await indexer.nfts({ addressBech32: sender.bech32 })).items;
-  const nftId = TransactionHelper.resolveIdFromOutputId(items[0]);
-
-  const blockId = await wallet.sendNft(sender, config.tangleRequestBech32);
-  await getLedgerInclusionState(blockId, wallet.client);
-
-  await mintMetadataNft({ name: 'Test2', someValue: 'test_test2' }, nftId);
+  const { nftId } = await mintMetadataNft({ name: 'Test', someValue: 'test' });
+  console.log('\n');
+  await mintMetadataNft({ name: 'Test2', someValue: 'test_test2' }, undefined, undefined, nftId);
 };
 
 exec();
